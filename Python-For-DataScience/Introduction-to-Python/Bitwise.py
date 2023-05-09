f573c28e888cb63a7daadf53609f63863c2b8ffc
Garvit Joshi <90491911+Garvitjoshi1@users.noreply.github.com>
2023-05-09 11:47:23 +0530

    Bitwise operator

--- Python-For-DataScience/Introduction-to-Python/Bitwise.py
+++ Python-For-DataScience/Introduction-to-Python/Bitwise.py
@@ -5,5 +5,5 @@ print(bin(a)) #0b1010
 print(bin(b)) #0b10100
 
 # convert binary to int
-print((int("0b10100", 2))) #20
-print((int("0b1010", 2))) #10
+print(int("0b10100", 2)) #20
+print(int("0b1010", 2)) #10
